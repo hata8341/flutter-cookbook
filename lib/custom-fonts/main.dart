3f9185589b40f77395e991a6513b7ba7f3801f38
hata <wmf8341@gmail.com>
2022-04-13 11:39:38 +0900

    custom-font

---
+++ lib/custom-fonts/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Fonts',
+      theme: ThemeData(fontFamily: 'Raleway'),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Custom Fonts')),
+      body: const Center(
+        child: Text(
+          'Roboto Mono sample',
+          style: TextStyle(fontFamily: 'RobotoMono'),
+        ),
+      ),
+    );
+  }
+}
