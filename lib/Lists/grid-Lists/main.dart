1b4d33a7d2e4c315e9b61ac816830fabfba636eb
hata <wmf8341@gmail.com>
2022-04-16 13:24:08 +0900

    grid-list

---
+++ lib/Lists/grid-Lists/main.dart
@@ -0,0 +1,33 @@
+// グリッドリストを表示
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Grid List';
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'アイテム $index',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
