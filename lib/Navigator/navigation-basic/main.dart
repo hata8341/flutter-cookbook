4212c48387ac06e090f7735ed43ce1b6627f9acc
hata <wmf8341@gmail.com>
2022-04-16 18:19:48 +0900

    navigation-basic

---
+++ lib/Navigator/navigation-basic/main.dart
@@ -0,0 +1,52 @@
+// ２つのルートを作成
+// navigator.pushで二番目の画面へ移動
+// navigator.popで一番目の画面へ戻る
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      title: 'navigation Basics',
+      home: FirstRoute(),
+    ));
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open route'),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const SecondRoute()));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('戻る'),
+        ),
+      ),
+    );
+  }
+}
